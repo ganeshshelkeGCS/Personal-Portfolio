e085fff7b1125f41694cb8585e9028690695f84d
Ganesh shelke <ganeshshelke785@gmail.com>
2024-05-07 14:49:16 +0530

    updated links

--- src/components/Projects/data/Data.jsx
+++ src/components/Projects/data/Data.jsx
@@ -9,7 +9,7 @@ const projects = [
       "Bringing Creativity to Life In this captivating React project, I embarked on a journey to showcase the power of creativity through interactive poster designs. Leveraging the versatility of React.js",
     image: ReactPoster,
     technologies: ["HTML ", "CSS ", "ReactJS ", "ExpressJS "],
-    explore: "https://github.com/ganeshshelkeGCS/FluxRepository",
+    explore: "https://github.com/ganeshshelkeGCS/ReactPoster",
   },
   {
     title: "Doctors At Door Step",
@@ -17,7 +17,7 @@ const projects = [
       "'Doctor at Doorstep' is a pioneering project aimed at revolutionizing healthcare accessibility by bringing medical expertise directly to the doorstep of individuals. With the days of long waiting times in crowded clinics ",
     image: Doctor,
     technologies: ["HTML ", "CSS ", "AngularJS ", "Java ", "MySQL"],
-    explore: "https://github.com/ganeshshelkeGCS/FluxRepository",
+    explore: "https://github.com/ganeshshelkeGCS/ReactPoster",
   },
   {
     title: "TextUtils",
@@ -25,7 +25,7 @@ const projects = [
       "Transform text effortlessly with TextUtils: convert to uppercase, lowercase, and beyond. Streamline your text processing tasks with ease!",
     image: TextUtils,
     technologies: ["HTML ", "CSS ", "ReactJS ", "BootStrap "],
-    explore: "https://github.com/ganeshshelkeGCS/FluxRepository",
+    explore: "https://github.com/ganeshshelkeGCS/TextUtilities",
   },
 ];
 
