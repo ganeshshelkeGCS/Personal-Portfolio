5339f52438700aedfc5d42d09da32df6f2f9a090
ganeshshelkeGCS <91302142+ganeshshelkeGCS@users.noreply.github.com>
2024-05-28 14:21:22 +0530

    added project styles

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,13 +4,16 @@ import styles from "./Projects.module.css";
 import projects from "./data/Data";
 
 export default function Projects() {
- 
   return (
-    <div className={styles.container} id="project" title="Click on Explore button to get more Info">
+    <div
+      className={styles.container}
+      id="project"
+      title="Click on Explore button to get more Info"
+    >
       <div className={styles.projectHeading}>
         <h1>Projects</h1>
       </div>
-      <div className={styles.projectContainer}>
+      {/* <div className={styles.projectContainer}>
        
         {projects.map((project, index) => (
           <div className={styles.project} key={index}>
@@ -23,9 +26,25 @@ export default function Projects() {
               <p>{project.description}</p>
               <p className={styles.projectTechnology}>{project.technologies.join(", ")}</p>
             </div>
-          </div>
+          </div> 
         ))}
       </div>
+       */}
+<div className={styles.cardsContainer} >
+      {projects.map((project, index) => (
+        <div className={styles.card} key={index}>
+          <div className={styles.imageBox} >
+            <img src={project.image} alt={project.title} />
+          </div>
+          <div className={styles.content}>
+            <h2>{project.title}</h2>
+            <p>{project.description}</p>
+            <p>{project.technologies.join(", ")}</p>
+            <button className={styles.exploreProjects}><a href={project.explore} target="_blank">Explore</a></button>
+          </div>
+        </div>
+      ))}
+    </div>
     </div>
   );
 }
