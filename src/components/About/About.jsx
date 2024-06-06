a828b6b959a05d506824b363c1173dc1bf66e625
ganeshshelkeGCS <91302142+ganeshshelkeGCS@users.noreply.github.com>
2024-06-06 14:15:23 +0530

    Updated Screen Wise View

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,11 +8,10 @@ function About() {
   return (
     <>
       <div className={styles.aboutInfo}>
-          <p className={styles.aboutInfoP}>
             Aspiring React.js Developer Greetings! I am Ganesh Shelke, a
             passionate and dedicated individual stepping into the world of web
             development with a focus on React.js
-          </p>
+         
       </div>
     </>
   );
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,28 +4,33 @@ import Resume from "../Resume/Resume";
 
 import styles from "./Profile.module.css";
 import About from "../About/About";
+import LinkedIn from "../../images/LinkedIn.png";
+import GitHub from "../../images/gitHub.png";
+import Gmail from "../../images/gmail.png"
 
 export default function Profile() {
   return (
     <>
       <div className={styles.mainContainer} id="about">
-        <div className={styles.about}>
-          <About />
-        </div>
-        <div>
-          <div className={styles.leftContainer}>
-            <img title="Profile Photo" src={Photo} alt="Image Photo" className={styles.photo} />
-          </div>
-          <div className={styles.rightContainer} id="resume">
-            <span>Hey I A'm Ganesh Shelke</span>
-            <span title="Send Mail"><a href="mailto:ganeshshelke785@gmail.com" className={styles.decoration1} target="_blank">ganeshshelke785@gmail.com</a></span>
-            <span title="GitHub"><a href="https://github.com/ganeshshelkeGCS" className={styles.decoration2} target="_blank">GitHub</a></span>
-            <span title="LinkedIn"><a href="https://in.linkedin.com/in/ganesh-shelke-270773233" className={styles.decoration3} target="_blank">LinkedIn</a></span>
-            <span className={styles.resume} title="Resume">
-              <Resume />
+            <span className={styles.leftContainer}>
+              <About />
             </span>
-          </div>
-        </div>
+            <div className={styles.rightContainer}>
+              <img
+                title="Profile Photo"
+                src={Photo}
+                alt="Image Photo"
+                className={styles.photo}/>
+            </div>
+      </div>
+      <div className={styles.BelowrightContainer} id="resume">
+            <span>Hey I A'm Ganesh Shelke</span>
+            <span className={styles.resume} title="Resume"> <Resume /></span>
+           <span className={styles.contactIcomMain}>
+            <span title="Send Mail"><a href="mailto:ganeshshelke785@gmail.com" className={styles.decoration1} target="_blank"><img src={Gmail} alt="ganeshshelke785@gmail.com" className={styles.contactIcon}/></a></span>
+            <span title="GitHub"><a href="https://github.com/ganeshshelkeGCS" className={styles.decoration2} target="_blank"> <img src={GitHub} alt="Git-Hub" className={styles.contactIcon}/></a></span>
+           <span title="LinkedIn"><a href="https://in.linkedin.com/in/ganesh-shelke-270773233" className={styles.decoration3} target="_blank"> <img src={LinkedIn} alt="LinkedIn" className={styles.contactIcon}/> </a></span>
+           </span>
       </div>
     </>
   );
